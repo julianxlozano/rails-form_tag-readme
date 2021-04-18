f5c833a2ae22ce6c9555c00982409b784b28c82d
julianxlozano <julianxlozano@gmail.com>
2021-04-18 11:58:11 -0500

    commit tree 663e4a3ec0e224f116721a0ab000da90c4f40c18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new
+  #  @post = Post.create()
+  end
+
+  def create 
+    @post = Post.create(title:params[:post][:title],description:params[:post][:description])
+    @post.save 
+    redirect_to '/posts'
+  end
 end
